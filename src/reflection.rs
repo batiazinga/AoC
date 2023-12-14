97c4bbf9ad31deca8d9de96a0628b1cac0147762
Batiazinga <luc.nguyen-the@pm.me>
2023-12-14 08:03:33 +0100

    Day13.2

--- src/reflection.rs
+++ src/reflection.rs
@@ -72,6 +72,84 @@ fn summarize_one_map(m: &Vec<Vec<char>>) -> u64 {
     0
 }
 
+pub fn fix_smudges_and_summarize(input: &str) -> u64 {
+    let mut summary = 0u64;
+
+    let mut lines = input.lines();
+    while let Some(mut line) = lines.next() {
+        let mut m: Vec<Vec<char>> = Vec::new();
+        while !line.is_empty() {
+            let mut row: Vec<char> = Vec::new();
+            for c in line.chars() {
+                row.push(c);
+            }
+            m.push(row);
+
+            let line_opt = lines.next();
+            if line_opt.is_none() {
+                break;
+            }
+            line = line_opt.unwrap();
+        }
+        summary += fix_smudge_and_summarize_one_map(&m);
+    }
+
+    summary
+}
+
+fn fix_smudge_and_summarize_one_map(m: &Vec<Vec<char>>) -> u64 {
+    let num_rows = m.len();
+    if num_rows == 0 {
+        return 0;
+    }
+    let num_columns = if num_rows > 0 { m[0].len() } else { 0 };
+    if num_columns == 0 {
+        return 0;
+    }
+
+    for j in 1..num_columns {
+        let mut count_diff = 0u8;
+        let max = if num_columns - j > j {
+            j
+        } else {
+            num_columns - j
+        };
+        'outer: for k in 0..max {
+            for i in 0..num_rows {
+                if m[i][j - 1 - k] != m[i][j + k] {
+                    count_diff += 1;
+                }
+                if count_diff > 1 {
+                    break 'outer;
+                }
+            }
+        }
+        if count_diff == 1 {
+            return j as u64;
+        }
+    }
+
+    for i in 1..num_rows {
+        let mut count_diff = 0u8;
+        let max = if num_rows - i > i { i } else { num_rows - i };
+        'outer: for k in 0..max {
+            for j in 0..num_columns {
+                if m[i - 1 - k][j] != m[i + k][j] {
+                    count_diff += 1;
+                }
+                if count_diff > 1 {
+                    break 'outer;
+                }
+            }
+        }
+        if count_diff == 1 {
+            return 100 * i as u64;
+        }
+    }
+
+    0
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
--- tests/day13.rs
+++ tests/day13.rs
@@ -1,8 +1,14 @@
-use aoc2023::reflection::summarize;
+use aoc2023::reflection::{summarize, fix_smudges_and_summarize};
 use std::fs;
 
 #[test]
 fn day_12_1() {
     let input = fs::read_to_string("data/day13.txt").unwrap();
-    assert_eq!(summarize(&input), 0);
+    assert_eq!(summarize(&input), 34918);
+}
+
+#[test]
+fn day_12_2() {
+    let input = fs::read_to_string("data/day13.txt").unwrap();
+    assert_eq!(fix_smudges_and_summarize(&input), 33054);
 }
