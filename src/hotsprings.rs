954bc5fdd56e5645e7e4cf8c658650f55f8176a7
Batiazinga <luc.nguyen-the@pm.me>
2023-12-27 17:53:14 +0100

    Optimize day12 but still too slow

--- src/hotsprings.rs
+++ src/hotsprings.rs
@@ -45,20 +45,20 @@ impl ConditionRecord {
         record
     }
 
-    pub fn unfold(&self, n: u64) -> ConditionRecord {
+    pub fn unfold(&self, n: usize) -> ConditionRecord {
         let mut record = ConditionRecord {
             states: Vec::with_capacity((self.states.len() + 1) * (n as usize) - 1),
-            group_sizes: Vec::with_capacity(self.group_sizes.len() * 5),
+            group_sizes: Vec::with_capacity(self.group_sizes.len() * n),
         };
 
-        for i in 0..5 {
+        for i in 0..n {
             for state in self.states.iter() {
                 record.states.push(state.clone());
             }
             for group_size in self.group_sizes.iter() {
                 record.group_sizes.push(*group_size);
             }
-            if i != 4 {
+            if i != n - 1 {
                 record.states.push(State::Unknown);
             }
         }
@@ -66,156 +66,96 @@ impl ConditionRecord {
         record
     }
 
-    fn num_damaged(&self) -> usize {
-        self.group_sizes.iter().sum()
-    }
-
-    fn num_known_damaged(&self) -> usize {
-        let mut count = 0;
-        self.states.iter().for_each(|s| match s {
-            State::Damaged => count += 1,
-            _ => {}
-        });
-        count
-    }
-
-    fn num_unknown(&self) -> usize {
-        let mut count = 0;
-        self.states.iter().for_each(|s| match s {
-            State::Unknown => count += 1,
-            _ => {}
-        });
-        count
-    }
-
-    fn is_valid(&self, candidate: &[State]) -> bool {
-        let mut candidate_cursor = 0usize;
-        let mut completed: Vec<State> = Vec::with_capacity(self.states.len());
-        for state in &self.states {
-            match state {
-                State::Unknown => {
-                    completed.push(candidate[candidate_cursor].clone());
-                    candidate_cursor += 1;
-                }
-                _ => completed.push(state.clone()),
-            }
-        }
-
-        group_sizes(completed.as_slice()) == self.group_sizes
-    }
-
-    fn is_valid_so_far(&self, candidate: &[State]) -> bool {
-        let mut candidate_cursor = 0usize;
-        let mut partial: Vec<State> = Vec::with_capacity(self.states.len());
-        for state in &self.states {
-            if state == &State::Unknown && candidate_cursor < candidate.len() {
-                partial.push(candidate[candidate_cursor].clone());
-                candidate_cursor += 1;
-            } else {
-                partial.push(state.clone());
-            }
-        }
-
-        let candiate_group_sizes = group_sizes(partial.as_slice());
-        for i in 0..candiate_group_sizes.len() {
-            if i < candiate_group_sizes.len() - 1 {
-                if candiate_group_sizes[i] != self.group_sizes[i] {
-                    return false;
-                }
-            } else {
-                if candiate_group_sizes[i] > self.group_sizes[i] {
-                    return false;
-                }
-            }
-        }
-        true
-    }
-
     pub fn count_arrangements(&self) -> u64 {
-        let mut count = 0u64;
-        self.generate_candidates_and_count(
-            self.num_damaged() - self.num_known_damaged(),
-            self.num_unknown(),
-            &mut count,
-        );
+        let mut count = 0;
+        let mut groups: Vec<PositionedGroup> = Vec::with_capacity(self.group_sizes.len());
+        self.rec_count_arrangements(&mut groups, 0, &mut count);
 
         count
     }
 
-    fn generate_candidates_and_count(
+    fn rec_count_arrangements(
         &self,
-        num_damaged_left: usize,
-        num_unknown: usize,
-        counter: &mut u64,
+        groups: &mut Vec<PositionedGroup>,
+        cursor: usize,
+        count: &mut u64,
     ) {
-        let mut candidate: Vec<State> = vec![State::Unknown; num_unknown as usize];
-        self.rec_generate_candidates_and_count(num_damaged_left, &mut candidate, 0, counter);
+        if groups.len() == self.group_sizes.len() {
+            *count += 1;
+            return;
+        }
+
+        let group_size = self.group_sizes[groups.len()];
+        let required_size = if groups.len() == self.group_sizes.len() {
+            group_size
+        } else {
+            group_size + 1
+        };
+        let min_required_size: usize = self.group_sizes[groups.len()..].iter().sum::<usize>()
+            + (self.group_sizes.len() - groups.len() - 1);
+        for i in cursor..self.states.len() - min_required_size + 1 {
+            groups.push(PositionedGroup {
+                size: group_size,
+                position: i,
+            });
+
+            if self.is_valid(groups.as_slice()) {
+                self.rec_count_arrangements(groups, i + required_size, count);
+            }
+
+            groups.pop();
+        }
     }
 
-    fn rec_generate_candidates_and_count(
-        &self,
-        num_damaged_left: usize,
-        candidate: &mut Vec<State>,
-        cursor: usize,
-        counter: &mut u64,
-    ) {
-        if cursor == candidate.len() {
-            if self.is_valid(candidate.as_slice()) {
-                *counter += 1;
+    fn is_valid(&self, groups: &[PositionedGroup]) -> bool {
+        if groups.len() == 0 {
+            return true;
+        }
+
+        for i in 0..groups[0].position {
+            if self.states[i] == State::Damaged {
+                return false;
             }
-            return;
         }
 
-        if !self.is_valid_so_far(candidate.as_slice()) {
-            return;
+        for i in 0..groups.len() {
+            let group = &groups[i];
+            for j in 0..group.size {
+                if self.states[group.position + j] == State::Operational {
+                    return false;
+                }
+            }
+            if i < groups.len() - 1 {
+                let next_group = &groups[i + 1];
+                for j in (group.position + group.size)..next_group.position {
+                    if self.states[j] == State::Damaged {
+                        return false;
+                    }
+                }
+            }
         }
 
-        if num_damaged_left > 0 {
-            candidate[cursor] = State::Damaged;
-            self.rec_generate_candidates_and_count(
-                num_damaged_left - 1,
-                candidate,
-                cursor + 1,
-                counter,
-            );
-            candidate[cursor] = State::Unknown;
+        let last_group = &groups[groups.len() - 1];
+        if groups.len() == self.group_sizes.len() {
+            for i in last_group.position + last_group.size..self.states.len() {
+                if self.states[i] == State::Damaged {
+                    return false;
+                }
+            }
+        } else {
+            if self.states[last_group.position + last_group.size] == State::Damaged {
+                return false;
+            }
         }
-        if candidate.len() - cursor > num_damaged_left as usize {
-            candidate[cursor] = State::Operational;
-            self.rec_generate_candidates_and_count(
-                num_damaged_left,
-                candidate,
-                cursor + 1,
-                counter,
-            );
-            candidate[cursor] = State::Unknown;
-        }
+
+        true
     }
 }
 
-fn group_sizes(states: &[State]) -> Vec<usize> {
-    let mut sizes: Vec<usize> = Vec::new();
-
-    let mut size = 0usize;
-    for s in states {
-        match s {
-            State::Damaged => size += 1,
-            State::Operational => {
-                if size != 0 {
-                    sizes.push(size);
-                    size = 0;
-                }
-            }
-            State::Unknown => {
-                break;
-            }
-        }
-    }
-    if size != 0 {
-        sizes.push(size);
-    }
-
-    sizes
+#[derive(Debug)]
+struct PositionedGroup {
+    size: usize,
+    position: usize,
 }
 
 pub fn read_records(input: &str) -> Vec<ConditionRecord> {
@@ -231,64 +171,6 @@ pub fn read_records(input: &str) -> Vec<ConditionRecord> {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_can_read_condition_record() {
-        let input = "???.### 1,1,3";
-        let record = ConditionRecord::parse(&input);
-
-        assert_eq!(record.num_damaged(), 5);
-        assert_eq!(record.num_unknown(), 3);
-    }
-
-    #[test]
-    fn test_compute_group_sizes() {
-        assert_eq!(
-            group_sizes(&[State::Damaged, State::Operational, State::Damaged]),
-            &[1, 1]
-        );
-        assert_eq!(
-            group_sizes(&[
-                State::Damaged,
-                State::Operational,
-                State::Damaged,
-                State::Operational,
-                State::Damaged,
-                State::Damaged,
-                State::Damaged
-            ]),
-            &[1, 1, 3]
-        );
-        assert_eq!(
-            group_sizes(&[
-                State::Operational,
-                State::Damaged,
-                State::Operational,
-                State::Damaged,
-                State::Unknown,
-                State::Damaged
-            ]),
-            &[1, 1]
-        );
-    }
-
-    #[test]
-    fn test_is_valid_candidate() {
-        let record = ConditionRecord::parse("???.### 1,1,3");
-
-        assert!(record.is_valid(&[State::Damaged, State::Operational, State::Damaged]));
-        assert!(!record.is_valid(&[State::Operational, State::Operational, State::Damaged]));
-    }
-
-    #[test]
-    fn test_is_valid_so_far() {
-        let record = ConditionRecord::parse("?#?#?#?#?#?#?#? 1,3,1,6");
-
-        assert!(record.is_valid_so_far(&[State::Operational]));
-        assert!(!record.is_valid_so_far(&[State::Damaged]));
-        assert!(record.is_valid_so_far(&[State::Operational, State::Operational]));
-        assert!(!record.is_valid_so_far(&[State::Operational, State::Damaged]));
-    }
 
     #[test]
     fn test_count_arrangements_1() {
@@ -356,9 +238,9 @@ mod tests {
         assert_eq!(record.unfold(5).count_arrangements(), 2500);
     }
 
-    // #[test]
-    // fn test_unfolded_count_arrangements_6() {
-    //     let record = ConditionRecord::parse("?###???????? 3,2,1");
-    //     assert_eq!(record.unfold(5).count_arrangements(), 506250);
-    // }
+    #[test]
+    fn test_unfolded_count_arrangements_6() {
+        let record = ConditionRecord::parse("?###???????? 3,2,1");
+        assert_eq!(record.unfold(5).count_arrangements(), 506250);
+    }
 }
--- tests/day12.rs
+++ tests/day12.rs
@@ -15,8 +15,8 @@ fn day_12_1() {
 // fn day_12_2() {
 //     let input = fs::read_to_string("data/day12.txt").unwrap();
 //     let mut sum = 0u64;
-//     for line in input.lines() {
-//         sum += ConditionRecord::parse(line).unfold(5).count_arrangements();
+//     for record in read_records(&input) {
+//         sum += record.unfold(3).count_arrangements();
 //     }
 //     assert_eq!(sum, 0);
 // }
